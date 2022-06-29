5a868488ce7e854351c8e7f77c5141fa6ce3f5c7
Brian Manoff <brian.a.manoff@gmail.com>
2022-06-29 14:56:42 -0700

    finished data aquisition exercises

---
+++ acquire.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+import os
+import env
+
+
+def get_connection(db, user=env.user, host=env.host, password=env.password):
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+def get_titanic_data(data):
+    filename = "titanic.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        # read the SQL query into a dataframe
+        df = pd.read_sql('SELECT * FROM passengers', get_connection('titanic_db'))
+
+        # Write that dataframe to disk for later. Called "caching" the data for later.
+        df.to_file(filename)
+
+        # Return the dataframe to the calling code
+        return df  
+
+
+def get_iris_data(data):
+    filename = "iris.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        # read the SQL query into a dataframe
+        df = pd.read_sql('SELECT * FROM measurements JOIN species USING (species_id)', get_connection('iris_db'))
+
+        # Write that dataframe to disk for later. Called "caching" the data for later.
+        df.to_file(filename)
+
+        # Return the dataframe to the calling code
+        return df  
+        
+
+def get_telco_data(data):
+    filename = "telco_churn.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        # read the SQL query into a dataframe
+        df = pd.read_sql('select * FROM customers JOIN contract_types using (contract_type_id) JOIN internet_service_types using (internet_service_type_id) JOIN payment_types using (payment_type_id)', get_connection('telco_churn'))
+
+        # Write that dataframe to disk for later. Called "caching" the data for later.
+        df.to_file(filename)
+
+        # Return the dataframe to the calling code
+        return df  
