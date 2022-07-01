bdab0f256a3db0ea8b5d908d268c5b3d7a735ef1
Brian Manoff <brian.a.manoff@gmail.com>
2022-07-01 14:55:24 -0700

    finished tidy data exercises

--- prepare.py
+++ prepare.py
@@ -3,6 +3,14 @@ import seaborn as sns
 import pandas as pd
 import acquire
 
+# import splitting and imputing functions
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+
+# turn off pink boxes for demo
+import warnings
+warnings.filterwarnings("ignore")
+
 def prep_iris():
     df = acquire.get_iris_data()
     df = df.drop(columns=['species_id', 'measurement_id'], axis = 1)
@@ -20,11 +28,39 @@ def prep_titanic():
     df = pd.concat([df, dummy_df], axis=1)
     return df
 
-def prep_telco():
+def prep_telco_data():
 
     df = acquire.get_telco_data()
-    df = df.drop_duplicates()
-    df = df.drop(columns=['Unnamed: 0', 'payment_type_id', 'internet_service_type_id', 'contract_type_id', 'customer_id'])
-    #dummy_df = pd.get_dummies(df[[]], drop_first=True)
-    #df = pd.concat([df, dummy_df], axis=1)
-    return df
+    # Drop duplicate columns
+    df.drop(columns=['payment_type_id', 'internet_service_type_id', 'contract_type_id', 'customer_id'], inplace=True)
+       
+    # Drop null values stored as whitespace    
+    df['total_charges'] = df['total_charges'].str.strip()
+    df = df[df.total_charges != '']
+    
+    # Convert to correct datatype
+    df['total_charges'] = df.total_charges.astype(float)
+    
+    # Convert binary categorical variables to numeric
+    df['gender_encoded'] = df.gender.map({'Female': 1, 'Male': 0})
+    df['partner_encoded'] = df.partner.map({'Yes': 1, 'No': 0})
+    df['dependents_encoded'] = df.dependents.map({'Yes': 1, 'No': 0})
+    df['phone_service_encoded'] = df.phone_service.map({'Yes': 1, 'No': 0})
+    df['paperless_billing_encoded'] = df.paperless_billing.map({'Yes': 1, 'No': 0})
+    df['churn_encoded'] = df.churn.map({'Yes': 1, 'No': 0})
+    
+    # Get dummies for non-binary categorical variables
+    dummy_df = pd.get_dummies(df[['multiple_lines', \
+                              'online_security', \
+                              'online_backup', \
+                              'device_protection', \
+                              'tech_support', \
+                              'streaming_tv', \
+                              'streaming_movies', \
+                              'contract_type', \
+                              'internet_service_type', \
+                              'payment_type']], dummy_na=False, \
+                              drop_first=True)
+    
+    # Concatenate dummy dataframe to original 
+    df = pd.concat([df, dummy_df], axis=1)
